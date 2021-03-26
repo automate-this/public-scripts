805e38c486d6e2ee2ded60fd60af54508f1755d4
automate-this <81413189+automate-this@users.noreply.github.com>
2021-03-26 10:29:22 +0100

    Create Get-FolderPermissions.ps1

---
+++ Office 365 - Exchange Online/Get-FolderPermissions.ps1
@@ -0,0 +1,14 @@
+$targetmailbox=<MailBox>
+ForEach($f in (Get-MailboxFolderStatistics $targetmailbox | Where {$_.FolderPath.StartsWith("/Posteingang")})) {
+ $fname = $targetmailbox + ":" + $f.FolderPath.Replace("/","\");
+ write-host $fname -BackgroundColor "Green" -ForegroundColor "Black"
+ $userlistArray = @()
+ ForEach($g in (Get-MailboxFolderPermission $fname | select User,AccessRights)){
+ if ($g.User -like "Standard") {
+   write-host " " $g.User ":" $g.AccessRights -BackgroundColor "White" -ForegroundColor "Black"
+  }
+ else {
+   write-host " " $g.User ":" $g.AccessRights
+  }
+ }
+}
