14b0bd0108b3849248da2098fba43e268c5e95d0
automate-this <81413189+automate-this@users.noreply.github.com>
2021-04-19 09:47:28 +0200

    Update Get-FolderPermissions.ps1

--- Office 365 - Exchange Online/Get-FolderPermissions.ps1
+++ Office 365 - Exchange Online/Get-FolderPermissions.ps1
@@ -1,14 +1,14 @@
-$targetmailbox=<MailBox>
+$targetmailbox = <Mailbox>
 ForEach($f in (Get-MailboxFolderStatistics $targetmailbox | Where {$_.FolderPath.StartsWith("/Posteingang")})) {
  $fname = $targetmailbox + ":" + $f.FolderPath.Replace("/","\");
- write-host $fname -BackgroundColor "Green" -ForegroundColor "Black"
- $userlistArray = @()
- ForEach($g in (Get-MailboxFolderPermission $fname | select User,AccessRights)){
- if ($g.User -like "Standard") {
-   write-host " " $g.User ":" $g.AccessRights -BackgroundColor "White" -ForegroundColor "Black"
-  }
- else {
-   write-host " " $g.User ":" $g.AccessRights
-  }
- }
+ write-host $fname -BackgroundColor "Green" -ForegroundColor "Black"
+ $userlistArray = @()
+ ForEach($g in (Get-MailboxFolderPermission $fname | select User,AccessRights)){
+ if ($g.User -like "Standard" -Or $g.User -like "Anonym") {
+   write-host " " $g.User ":" $g.AccessRights -BackgroundColor "White" -ForegroundColor "Black"
+  }
+ else {
+   write-host " " $g.User ":" $g.AccessRights
+  }
+ }
 }
