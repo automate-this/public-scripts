c627f05ace40eaecbd0caf5ddeca4ca900c16e50
automate-this <81413189+automate-this@users.noreply.github.com>
2021-04-19 09:48:21 +0200

    Update Set-FolderPermissions.ps1

--- Office 365 - Exchange Online/Set-FolderPermissions.ps1
+++ Office 365 - Exchange Online/Set-FolderPermissions.ps1
@@ -1,21 +1,21 @@
 #Reset Permissions
-cls
-$targetmailbox=<MailBox>
+
+$targetmailbox=<Mailbox>
 ForEach($f in (Get-MailboxFolderStatistics $targetmailbox | Where { $_.FolderPath.StartsWith("/Posteingang")})) {
  $fname = $targetmailbox + ":" + $f.FolderPath.Replace("/","\");
  $userlistArray = @()
  #Purge existing permsissions and set defaults
  ForEach($g in (Get-MailboxFolderPermission $fname | select User,AccessRights)) {
- if ($g.User -like "Standard") {
-   Set-MailboxFolderPermission $fname -User $g.User -AccessRights None
+ if ($g.User.DisplayName -like "Standard" -Or $g.User.DisplayName -like "Anonym") {
+   Set-MailboxFolderPermission $fname -User $g.User.DisplayName -AccessRights None
   }
   else {
-   Remove-MailboxFolderPermission $fname -User $g.User -confirm: $false
+   Remove-MailboxFolderPermission $fname -User $g.User.DisplayName -confirm: $false
   }
  }
  #Set owner permissions (modify users and rights to assign other permission values to specific users)
  $user=(<User1>,<User2>,<User3>)
  ForEach ($targetuser in $user){
-  Add-MailboxFolderPermission $fname -User $targetuser -AccessRights Owner
+  Add-MailboxFolderPermission $fname -User $targetuser -AccessRights PublishingEditor
  }
 }
